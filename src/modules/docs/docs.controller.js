31a78b89e302c679c6ff312de97488d1359fe119
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-07-15 23:39:24 +0200

    chore: reflect changes in .gitignore

--- src/modules/docs/docs.controller.js
+++ src/modules/docs/docs.controller.js
@@ -1,7 +1,21 @@
+const enigma = require('./../../../node_modules/enigma.js/');
+const WebSocket = require('ws');
+const schema = require('enigma.js/schemas/12.20.0.json');
 
-class HealthController {
+class DocsController {
 
   static get(req, res, next) {
+
+    const session = enigma.create({
+      schema,
+      url: 'ws://localhost:9076/app',
+      createSocket: url => new WebSocket(url)
+    });
+
+    // Bind traffic events to log what is sent and received on the socket:
+    session.on('traffic:sent', data => console.log('sent:', data));
+    session.on('traffic:received', data => console.log('received:', data));
+
     res.setHeader('Content-Type', 'application/json');
     res.send({
       ts: new Date().toJSON()
@@ -11,4 +25,4 @@ class HealthController {
 
 }
 
-module.exports = HealthController;
+module.exports = DocsController;
