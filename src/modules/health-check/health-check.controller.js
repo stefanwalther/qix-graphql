86c841266e36a07c432667e8998499f28f7ced90
Stefan Walther <swr@qlik.com>
2018-05-10 00:09:59 +0200

    refactor: simplify health-check implementation

--- src/modules/health-check/health-check.controller.js
+++ src/modules/health-check/health-check.controller.js
@@ -2,16 +2,14 @@ const pkg = require('./../../../package.json');
 
 class HealthController {
 
-  static get(req, res, next) {
+  static get(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
       ts: new Date().toJSON(),
       version: pkg.version,
       name: pkg.name
     });
-    next();
   }
-
 }
 
 module.exports = HealthController;
