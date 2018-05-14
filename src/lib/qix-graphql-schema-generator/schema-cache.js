33c30cf6e81f93d76c733682c56bdca3182d8ace
Stefan Walther <swr@qlik.com>
2018-05-15 00:59:01 +0200

    refactor: create a separate schema cache

---
+++ src/lib/qix-graphql-schema-generator/schema-cache.js
@@ -0,0 +1,41 @@
+
+// The idea of a schema cache has been introduced here as generating a schema is pretty expensive as the app
+// has to be opened with data, opening the data without the data will not return the table_and_keys properly.
+//
+// For now, this is a very simple implementation, which can
+// a) either be completely removed if the engine can return the meta data without opening the app with data
+// b) be extended in a multi-server environment with e.g. a Redis cache
+
+class SchemaCache {
+  constructor() {
+    this.cache = {};
+  }
+
+  find(qDocId) {
+    if (this.cache[qDocId]) {
+      return this.cache[qDocId];
+    }
+  }
+
+  exists(qDocId) {
+    return (this.cache[qDocId] === null);
+  }
+
+  /**
+   * Adds or replaces a cache.
+   * @param qDocId
+   */
+  add(qDocId, val) {
+    this.cache[qDocId] = val;
+  }
+
+  count() {
+    return Object.keys(this.cache).length;
+  }
+
+  reset() {
+    this.cache = {};
+  }
+}
+
+module.exports = new SchemaCache();
---
+++ test/unit/schema-cache.spec.js
@@ -0,0 +1,32 @@
+const schemaCache = require('./../../src/lib/qix-graphql-schema-generator/schema-cache');
+
+describe('UNIT => schema-cache', () => {
+
+  beforeEach(() => {
+    schemaCache.reset();
+  });
+
+  it('exists', () => {
+    expect(schemaCache).to.exist;
+  });
+
+  it('has a property cache', () => {
+    expect(schemaCache).to.have.property('cache').to.exist;
+  });
+
+  it('has zero cache objects by default', () => {
+    expect(schemaCache.count()).to.equal(0);
+  });
+
+  it('allows to add an object to the cache', () => {
+    const qDocId = 'foo';
+    const obj = {
+      foo: 'bar'
+    };
+    expect(schemaCache.count()).to.be.equal(0);
+    schemaCache.add(qDocId, obj);
+    expect(schemaCache.count()).to.be.equal(1);
+    expect(schemaCache.find(qDocId)).to.be.equal(obj);
+  })
+
+});
