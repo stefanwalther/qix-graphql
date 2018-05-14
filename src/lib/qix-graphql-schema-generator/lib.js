b261d0c620f0b6b8b001fc1ec5509314835c9df3
Stefan Walther <swr@qlik.com>
2018-05-15 00:59:47 +0200

    style: add some code comments

--- src/lib/qix-graphql-schema-generator/lib.js
+++ src/lib/qix-graphql-schema-generator/lib.js
@@ -1,7 +1,16 @@
 
 module.exports = {
 
-  // Todo: document
+  /**
+   * Normalize table-names and field-names to be used in the GraphQL editor.
+   *
+   * Spaces and special characters will all be replaced with the underscore character ("_").
+   *
+   * @Todo: The entire function should probable be renamed to "sanitize" which better describes what it does.
+   *
+   * @param {String} s - String to be checked.
+   * @returns {String} - The normalized string.
+   */
   normalize: s => {
     s = s.replace(/ /g, '_'); // eslint-disable-line no-useless-escape
     s = s.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '_'); // eslint-disable-line no-useless-escape
