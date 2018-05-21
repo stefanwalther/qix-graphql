d67a9b26f3110c747776e496fc0f6bda3e70d533
Stefan Walther <swr@qlik.com>
2018-05-21 19:32:38 +0200

    refactor: remove unused prototyping methods

--- src/modules/app/z-sample-schema.js
+++ src/modules/app/z-sample-schema.js
@@ -5,43 +5,8 @@
 
 const logger = require('winster').instance();
 const SchemaGenerator = require('./../../lib/qix-graphql-schema-generator/schema-generator');
-const {
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString
-} = require('graphql');
 
 let schemas = {};
-
-// Todo: to delete, just prototyping
-const schema1 = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'root',
-    fields: {
-      doc: {
-        type: GraphQLString,
-        resolve: (/* obj, args, ctx */) => {
-          return 'schema1 doc';
-        }
-      }
-    }
-  })
-});
-
-// Todo: to delete, just prototyping
-const schema2 = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'root',
-    fields: {
-      doc: {
-        type: GraphQLString,
-        resolve: (/* obj, args, ctx */) => {
-          return 'schema2 doc';
-        }
-      }
-    }
-  })
-});
 
 // Todo: This is where we could introduce a cache which can scale out, e.g. Redis
 const genSchema = qDocId => {
@@ -65,7 +30,5 @@ const genSchema = qDocId => {
 };
 
 module.exports = {
-  schema1,
-  schema2,
   genSchema
 };
