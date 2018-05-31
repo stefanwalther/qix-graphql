dddc9fcbbd8d0b8b165a18cdc2e97af0e8e9d2cb
Stefan Walther <swr@qlik.com>
2018-06-01 01:14:47 +0200

    test: provide some clarification

--- examples/node-js/test.spec.js
+++ examples/node-js/test.spec.js
@@ -5,7 +5,6 @@ const script = require('./index');
 describe('Run the test', () => {
   it('returns a collection of docs', async () => {
     let result = await script.getDocs();
-    console.log(result);
     expect(result).to.exist;
     expect(result).to.have.a.property('data');
     expect(result.data).to.have.a.property('docs');
---
+++ examples/test/build-tests.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+## Scripts to build all the tests, to then be included in the CircleCI tests
